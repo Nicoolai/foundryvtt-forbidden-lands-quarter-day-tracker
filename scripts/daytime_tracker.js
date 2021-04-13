be511e3c73c3e8247824906ef394a654c8ce7ab9
Nicolai Søndergaard <Nicolai.Sondergaard@gmail.com>
2021-04-13 22:09:22 +0200

    sdf

--- scripts/daytime_tracker.js
+++ scripts/daytime_tracker.js
@@ -14,27 +14,26 @@ Hooks.on('setup', () => {
 
 Hooks.on('ready', () => {
     // is this where we load?
-    game.daytime_tracker = new DaytimeTracker();
+    //game.daytime_tracker = new DaytimeTracker();
 });
 
 Hooks.on('getSceneControlButtons', (controls) => {
-    console.log(JSON.stringify(controls));
-    // controls.find((c) => c.name === 'token').tools.push({
-    //     name: 'EmitterEditor',
-    //     title: game.i18n.localize('PARTICLEFX.EmitterEditor'),
-    //     icon: 'fas fa-fire',
-    //     onClick() {
-    //         new ParticleFXApplication(
-    //             { title: 'ParticleFX Emitter Editor' }, {
-    //             classes: 'particlefx-editor',
-    //             width: 750,
-    //             height: 750,
-    //             resizable: true,
-    //         }, 'EmitterEditor',
-    //         );
-    //     },
-    //     button: true,
-    // });
+    controls.find((c) => c.name === 'token').tools.push({
+        name: 'DaylightTracker',
+        title: game.i18n.localize('DAYTRACKER.Title'),
+        icon: 'fas fa-sun-o',
+        // onClick() {
+        //     new ParticleFXApplication(
+        //         { title: 'ParticleFX Emitter Editor' }, {
+        //         classes: 'particlefx-editor',
+        //         width: 750,
+        //         height: 750,
+        //         resizable: true,
+        //     }, 'EmitterEditor',
+        //     );
+        // },
+        button: true,
+    });
 });
 
 export class DaytimeTracker {
